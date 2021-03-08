d72ab834d6791a4cc03f088c353fd461c42c8221
janice880624 <47550853+janice880624@users.noreply.github.com>
2021-03-08 23:13:07 +0800

    ex1

--- ex1.py
+++ ex1.py
@@ -3,11 +3,20 @@ import cv2
 # 灰階模式(單通道)
 img = cv2.imread('photo_video/lena.jpg', 0)
 print(img.shape)
+# (512, 512)
 
-# 彩色(三通道)
+# 彩色(三通道) -> 預設
 img2 = cv2.imread('photo_video/lena.jpg', 1)
 print(img2.shape)
+# (512, 512, 3)
 
 # 彩色(透過解碼)
 img3 = cv2.imread('photo_video/lena.jpg', -1)
 print(img3.shape)
+# (512, 512, 3)
+
+cv2.imshow('photo_video/lena image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imwrite('photo_video/lena_copy.png', img)
