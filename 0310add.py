704c78495ef87e452678c0229dedaba1ca3619de
janice880624 <47550853+janice880624@users.noreply.github.com>
2021-03-10 15:52:07 +0800

    0310

--- 0310add.py
+++ 0310add.py
@@ -2,12 +2,12 @@ import cv2
 
 def click_event(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.rectangle(img, (384, 0), (510, 128), (0, 0, 255), 10)
+        cv2.rectangle(img, (x-20, y-20), (x+20, y+20), (0, 0, 255), 20)
         cv2.imshow('photo_video/image', img)
         
 
     if event == cv2.EVENT_LBUTTONUP:
-        cv2.circle(img, (447, 63), 63, (0, 255, 0), -1)
+        cv2.circle(img, (x, y), 20, (0, 255, 0), -1)
         cv2.imshow('photo_vodeo/image', img)
 
 img = cv2.imread('photo_video/lena.jpg')
