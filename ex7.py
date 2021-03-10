78d2bcd7b1069cb5d58ef7b7e4b34b289bf3e6a9
janice kuo <janice880624@gmail.com>
2021-03-10 13:52:11 +0800

    7

--- ex7.py
+++ ex7.py
@@ -4,4 +4,23 @@ def click_event(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDBLCLK:
         print(x, ',', y)
         font = cv2.FONT_HERSHEY_SIMPLEX
-        text = str(x) + ',' str(y)
+        text = str(x) + ',' + str(y)
+        cv2.putText(img, text, (x, y), font, 0.5, (255 ,255 ,0), 2, cv2.LINE_AA)
+        cv2.imshow('image', img)
+
+    if event == cv2.EVENT_RBUTTONDBLCLK:
+        blue = img[y, x, 0]
+        green = img[y, x, 1]
+        red = img[y, x, 2]
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        text = str(blue) + ',' + str(green) + ',' + str(red)
+        cv2.putText(img, text, (x, y), font, 0.5, (0 ,255 ,255), 2, cv2.LINE_AA)
+        cv2.imshow('image', img)
+
+img = cv2.imread('lena.jpg')
+cv2.imshow('image', img)
+
+cv2.setMouseCallback('image', click_event)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows
