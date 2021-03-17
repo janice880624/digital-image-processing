41f3879924fa190bcbfabc5bfdbb040e8e0a8aa0
janice880624 <47550853+janice880624@users.noreply.github.com>
2021-03-17 14:25:06 +0800

    14

--- ex14.py
+++ ex14.py
@@ -6,24 +6,24 @@ def nothing(x):
     #pass
 # Create a black image, a window
 img = np.zeros((300,512,3), np.uint8)
-cv.namedWindow('image')
+cv.namedWindow('photo_video/image')
 
-cv.createTrackbar('B', 'image', 0, 255, nothing)
-cv.createTrackbar('G', 'image', 0, 255, nothing)
-cv.createTrackbar('R', 'image', 0, 255, nothing)
+cv.createTrackbar('B', 'photo_video/image', 0, 255, nothing)
+cv.createTrackbar('G', 'photo_video/image', 0, 255, nothing)
+cv.createTrackbar('R', 'photo_video/image', 0, 255, nothing)
 
 switch = '0 : OFF\n 1 : ON'
-cv.createTrackbar(switch, 'image', 0, 1, nothing)
+cv.createTrackbar(switch, 'photo_video/image', 0, 1, nothing)
 
 while(1):
-    cv.imshow('photo_vodeo/image',img)
+    cv.imshow('photo_video/image',img)
     k = cv.waitKey(1) & 0xFF
     if k == 27: # esc key
         break
-    b = cv.getTrackbarPos('B', 'photo_vodeo/image')
-    g = cv.getTrackbarPos('G', 'photo_vodeo/image')
-    r = cv.getTrackbarPos('R', 'photo_vodeo/image')
-    s = cv.getTrackbarPos(switch, 'photo_vodeo/image')
+    b = cv.getTrackbarPos('B', 'photo_video/image')
+    g = cv.getTrackbarPos('G', 'photo_video/image')
+    r = cv.getTrackbarPos('R', 'photo_video/image')
+    s = cv.getTrackbarPos(switch, 'photo_video/image')
 
     if s == 0:
         img[:] = 0
